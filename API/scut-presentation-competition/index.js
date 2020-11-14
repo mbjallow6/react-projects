df1c2422a656377284e8116c09543b6b5150fc97
Luka <mbjallow6@gmail.com>
2020-11-14 23:48:19 +0800

    initial commit

---
+++ API/scut-presentation-competition/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const importData = require("./data.json");
+let port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send(importData);
+});
+
+app.listen(port, () => {
+    console.log(`listening at http://localhost:${port}`)
+});
