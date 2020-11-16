3a7fb6c526d4cbbf34d545651937d4c1940aa704
Luka <mbjallow6@gmail.com>
2020-11-16 20:12:38 +0800

    initial test

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -2,7 +2,16 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+ const [data1, setPeople] = useState(data);
+  return (
+    <main>
+      <section className = 'container'>
+        <h3> {data1.length } Birthdays today</h3>
+        <List people={data1}/>
+        <button onClick={()=>setPeople([])}>Clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 2-tours/final/src/App.js
+++ 2-tours/final/src/App.js
@@ -1,58 +1,80 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+import React, { useState, useEffect} from 'react'
+import ReactPlayer from 'react-player/lazy'
+import items from './data';
+// import Teams from './Teams';
 
-function App() {
-  const [loading, setLoading] = useState(true)
-  const [tours, setTours] = useState([])
+function App(){
+  const [teams, setTeams] = useState(items);
+  const [value, setValue] = useState(0);
 
-  const removeTour = (id) => {
-    const newTours = tours.filter((tour) => tour.id !== id)
-    setTours(newTours)
-  }
+ 
+  
+  
 
-  const fetchTours = async () => {
-    setLoading(true)
-    try {
-      const response = await fetch(url)
-      const tours = await response.json()
-      setLoading(false)
-      setTours(tours)
-    } catch (error) {
-      setLoading(false)
-      console.log(error)
-    }
-  }
-  useEffect(() => {
-    fetchTours()
-  }, [])
-  if (loading) {
-    return (
-      <main>
-        <Loading />
-      </main>
-    )
-  }
-  if (tours.length === 0) {
-    return (
-      <main>
-        <div className='title'>
-          <h2>no tours left</h2>
-          <button className='btn' onClick={() => fetchTours()}>
-            refresh
+  const {video,th, members,major,image} = teams[value]
+  return(
+    <section className='section'>
+      <div className="title">
+         <h3>SCUT PRESENTATION COMPETITION</h3>
+         <div className="underline"></div>
+      </div>
+     
+  
+
+    {/* side tab */}
+    <div className="tab-container">
+      {teams.map((item,index)=>{
+        return(
+          <button key = {item.id}
+          onClick={()=>setValue(index)}
+          className= {`team-btn ${index===value && 'active-btn'}`}
+          >
+            {item.team}
           </button>
-        </div>
-      </main>
+        )
+      })}
+      
+      </div>  
+    {/* contentent container */}
+    <div className="video-container">
+      <ReactPlayer
+          url={video}
+          controls = 'true'
+          light = {th}/>
+    </div>
+  
+    <div className="img-container">
+       {image.map((photo,index)=>{
+    return(
+        <img src={photo}/>
     )
-  }
-  return (
-    <main>
-      <Tours tours={tours} removeTour={removeTour} />
-    </main>
+  })}
+    </div>
+    
+     
+    <div className="name-container">
+      {members.map((member,index)=>{
+    return(
+        <h5 className ='names'>{member}</h5>
+    )
+  })}
+    </div>
+    
+      
+    <div className="major-container">
+      {major.map((course,index)=>{
+        return(
+     
+       <h6 className = 'majors'>{course}</h6>
+      )
+        })}
+    </div>
+    
+  
+  
+    </section>
   )
-}
+
+};
 
 export default App
